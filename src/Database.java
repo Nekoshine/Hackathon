2841d328a0acd04e476f882f17376e051b1f1565
Nekoshine <uharry44@gmail.com>
2019-03-16 22:40:34 +0100

    Database advanced

--- src/Database.java
+++ src/Database.java
@@ -4,21 +4,22 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Database {
 	private static Connection connexion;
 
 	private Database() {
-		connexion = getConnection();
+		connexion = getConnexion();
 	}
 
 	public static Connection getConnexion() {
 		try {
-		 Class.forName("com.mysql.jdbc.Driver");
-	 } catch(ClassNotFoundException e) {
-			 System.err.println("Unable to load MySQL Driver " + e.getMessage());
-		 e.printStackTrace();
-	 }
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch(ClassNotFoundException e) {
+			System.err.println("Unable to load MySQL Driver " + e.getMessage());
+			e.printStackTrace();
+		}
 		if (connexion == null) {
 			try {
 				System.out.println("Initialising connection to Database");
@@ -32,6 +33,46 @@ public class Database {
 		System.out.println("Getting connexion to database...");
 		return connexion;
 	}
+
+	public static int getRandom(int categorie){
+		Random rand = new Random();
+		int id=0;
+		try {
+			PreparedStatement requete = Database.getConnexion()
+					.prepareStatement("SELECT COUNT(*) FROM Question WHERE Categorie=?");
+			requete.setInt(1, categorie);
+			ResultSet resultat = requete.executeQuery();
+			resultat.next();
+			int count=resultat.getInt("count(*)");
+			id = rand.nextInt(count);
+			requete.close();
+			resultat.close();
+		} catch (SQLException e) {
+			System.err.println("Erreur connexion : " + e.getMessage());
+		}
+		return id;
+	}
+
+	public static Carte getCarte(int id,int categorie){
+		try {
+			PreparedStatement requete = Database.getConnexion()
+					.prepareStatement("SELECT * FROM Question WHERE IDQuestion=? and Categorie=?");
+			requete.setInt(1, id);
+			requete.setInt(2, categorie);
+			ResultSet resultat = requete.executeQuery();
+			while (resultat.next()) {
+				Carte carte = new Carte(resultat.getInt("id"), resultat.getString("nom"),
+						resultat.getString("prenom"));
+				return client;
+			}
+			requete.close();
+			resultat.close();
+		} catch (SQLException e) {
+			System.err.println("Erreur connexion : " + e.getMessage());
+		}
+		return null;
+	}
+
 	public static void main(String[] args) {
 		Database.getConnexion();
 	}
--- src/Hackathon.java
+++ src/Hackathon.java
@@ -3,7 +3,7 @@ import java.sql.SQLException;
 
 public class Hackathon {
     public static void main(String [] args){
-        Connection db = Database.getConnection();
+        Connection db = Database.getConnexion();
         try {
             System.out.println(db.getClientInfo());
         } catch (SQLException e) {
