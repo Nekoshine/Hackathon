882478da20ee34b255efd99f791864f956b16465
Colin <colinluciani@gmail.com>
2019-03-17 00:07:56 +0100

    situation

--- src/Game.java
+++ src/Game.java
@@ -6,20 +6,34 @@ public class Game {
     protected int turn;
 
     public Game() {
-        money = 0.5;
-        health = 0.5;
-        turn=0;
+        money = 50;
+        health = 50;
+        turn=1;
+        currentSituation = Database.getSituation(Database.getRandom(turn),turn);
     }
 
     public void chooseLeft(){
-        currentSituation = currentSituation.chooseLeft(this);
-        if(currentSituation==null) {
-
+        int nextId = currentSituation.chooseLeft(this);
+        if(nextId==0) {
+            turn++;
+            currentSituation = Database.getSituation(Database.getRandom(turn),turn);
+        } else {
+            currentSituation = Database.getSituation(nextId, 11);
         }
     }
 
     public void chooseRight(){
-        currentSituation = currentSituation.chooseRight(this);
-    }
+        int nextId = currentSituation.chooseRight(this);
+        if(nextId==0) {
+            turn++;
+            if(turn>10) {
+                // todo: afficher la fin
 
+            }
+            currentSituation = Database.getSituation(Database.getRandom(turn),turn);
+        } else {
+            currentSituation = Database.getSituation(nextId, 11);
+        }
+
+    }
 }
