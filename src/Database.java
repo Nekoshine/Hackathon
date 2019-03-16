f5e4ed920088eb3a2c9cfacb90b2b1b2eac07283
Nekoshine <uharry44@gmail.com>
2019-03-16 17:45:13 +0100

    Ajout Connexion DB

---
+++ src/Database.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Database {
+	private static Connection connexion;
+
+	private Database() {
+		getConnexion();
+	}
+
+	public static Connection getConnexion() {
+		if (connexion == null) {
+			try {
+				System.out.println("Initialising connection to Database");
+				Database.connexion = DriverManager.getConnection("jdbc:mariadb://localhost","unZgwZdVHJ", "9QMg89jteB");
+				System.out.println("Connection to Hostel Database complete");
+				return connexion;
+			} catch (SQLException e) {
+				System.err.println("Couldn't connect to DB : " + e.getMessage());
+			}
+		}
+		System.out.println("Getting connexion to database...");
+		return connexion;
+	}
+}
